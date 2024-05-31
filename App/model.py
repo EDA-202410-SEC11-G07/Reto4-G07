f74ad758db9dcacc52be7b92bc3d353624b06d84
JFelipeAcosta <j.acostau@uniandes.edu.co>
2024-05-31 15:30:50 -0500

    ,

--- App/model.py
+++ App/model.py
@@ -25,7 +25,10 @@
  """
 
 
+import folium
+from folium.plugins import MarkerCluster
 import config as cf
+import math
 from DISClib.ADT import list as lt
 from DISClib.ADT import stack as st
 from DISClib.ADT import queue as qu
@@ -62,53 +65,186 @@ def new_data_structs():
     manera vacía para posteriormente almacenar la información.
     """
     #TODO: Inicializar las estructuras de datos
-    pass
+    model = {
+        'graph_comercial_distance': None,
+        'graph_comercial_time': None,
+        'graph_charge_distance': None,
+        'graph_militar_time': None,
+        'graph_militar_distance': None,
+        'hash_airports': None,
+        'hash_routes': None,
+        'concurrency_comercial': None,
+        'concurrency_charge': None,
+        'concurrency_militar': None
+
+        
+    }
+    model['graph_comercial_distance'] = gr.newGraph(datastructure='ADJ_LIST', directed=True, size=500)
+    model['graph_comercial_time'] = gr.newGraph(datastructure='ADJ_LIST', directed=True, size=500)
+    model['graph_charge_distance'] = gr.newGraph(datastructure='ADJ_LIST', directed=True, size=500)
+    model['graph_militar_time'] = gr.newGraph(datastructure='ADJ_LIST', directed=True, size=500)
+    model['graph_militar_distance'] = gr.newGraph(datastructure='ADJ_LIST', directed=True, size=500)
+    model['hash_airports'] = mp.newMap(500, maptype='PROBING')
+    model['hash_routes'] = mp.newMap(500, maptype='PROBING')
+    model['concurrency_comercial'] = lt.newList('ARRAY_LIST')
+    model['concurrency_charge'] = lt.newList('ARRAY_LIST')
+    model['concurrency_militar'] = lt.newList('ARRAY_LIST')
+    return model
+
 
 
 # Funciones para agregar informacion al modelo
 
-def add_data(data_structs, data):
+def add_vertex(model, vertex):
     """
-    Función para agregar nuevos elementos a la lista
+    Agrega un vertice al grafo
     """
-    #TODO: Crear la función para agregar elementos a una lista
-    pass
+    vertex_name = vertex['ICAO']
+    gr.insertVertex(model['graph_comercial_distance'], vertex_name)
+    gr.insertVertex(model['graph_comercial_time'], vertex_name)
+    gr.insertVertex(model['graph_charge_distance'], vertex_name)
+    gr.insertVertex(model['graph_militar_time'], vertex_name)
+    gr.insertVertex(model['graph_militar_distance'], vertex_name)
+    if not mp.contains(model['hash_airports'], vertex_name):
+        vertex['concurrency_comercial'] = 0
+        vertex['concurrency_charge'] = 0
+        vertex['concurrency_militar'] = 0
+        mp.put(model['hash_airports'], vertex_name, vertex)
 
-
-# Funciones para creacion de datos
-
-def new_data(id, info):
+def add_edge(model, edge):
     """
-    Crea una nueva estructura para modelar los datos
+    Agrega un arco al grafo
     """
-    #TODO: Crear la función para estructurar los datos
-    pass
+
+    icao_origin = edge['ORIGEN']
+    icao_dest = edge['DESTINO']
+
+    vertex_origin = me.getValue(mp.get(model['hash_airports'], icao_origin))
+    vertex_dest = me.getValue(mp.get(model['hash_airports'], icao_dest))
+
+    distance = haversine(vertex_origin['LATITUD'], vertex_origin['LONGITUD'], vertex_dest['LATITUD'], vertex_dest['LONGITUD'])
+    edge['DISTANCIA'] = distance
+
+    if edge['TIPO_VUELO'] == 'AVIACION_COMERCIAL':
+        gr.addEdge(model['graph_comercial_distance'], icao_origin, icao_dest, distance)
+        gr.addEdge(model['graph_comercial_time'], icao_origin, icao_dest, edge['TIEMPO_VUELO'])
+        vertex_origin['concurrency_comercial'] += 1
+        vertex_dest['concurrency_comercial'] += 1
+    elif edge['TIPO_VUELO'] == 'AVIACION_CARGA':
+        gr.addEdge(model['graph_charge_distance'], icao_origin, icao_dest, distance)
+        vertex_origin['concurrency_charge'] += 1
+        vertex_dest['concurrency_charge'] += 1
+    elif edge['TIPO_VUELO'] == 'MILITAR':
+        gr.addEdge(model['graph_militar_time'], icao_origin, icao_dest, edge['TIEMPO_VUELO'])
+        gr.addEdge(model['graph_militar_distance'], icao_origin, icao_dest, distance)
+        vertex_origin['concurrency_militar'] += 1
+        vertex_dest['concurrency_militar'] += 1
+    else:
+        print('Tipo de vuelo no reconocido')
+    
+    id_hash = icao_origin + '-' + icao_dest + '-' + edge['TIPO_VUELO']
+    mp.put(model['hash_routes'], id_hash, edge)
+
+def concurrencies(data_structs):
+    
+    list_comercial = data_structs['concurrency_comercial']
+    list_cargo = data_structs['concurrency_charge']
+    list_militar = data_structs['concurrency_militar']
+
+    list_general = mp.valueSet(data_structs['hash_airports'])
+
+    for airport in lt.iterator(list_general):
+        if airport['concurrency_comercial'] > 0:
+            lt.addLast(list_comercial, airport)
+        if airport['concurrency_charge'] > 0:
+            lt.addLast(list_cargo, airport)
+        if airport['concurrency_militar'] > 0:
+            lt.addLast(list_militar, airport)
+
+    sort_files(list_comercial, 'AVIACION_COMERCIAL')
+    sort_files(list_cargo, 'AVIACION_CARGA')
+    sort_files(list_militar, 'MILITAR')
 
 
-# Funciones de consulta
 
-def get_data(data_structs, id):
+
+def haversine(lat1, lon1, lat2, lon2):
     """
-    Retorna un dato a partir de su ID
+    Calcula la distancia entre dos puntos en la superficie terrestre
     """
-    #TODO: Crear la función para obtener un dato de una lista
-    pass
+    conversion = math.pi/180
+    lat1 = lat1 * conversion
+    lon1 = lon1 * conversion
+    lat2 = lat2 * conversion
+    lon2 = lon2 * conversion
+
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+
+    a = (math.sin(dlat/2))**2 + (math.cos(lat1) * math.cos(lat2) * (math.sin(dlon/2))**2)
+    c = 2 * math.asin(math.sqrt(a))
+    d = 6371 * c
+    return d
 
 
-def data_size(data_structs):
-    """
-    Retorna el tamaño de la lista de datos
-    """
-    #TODO: Crear la función para obtener el tamaño de una lista
-    pass
 
 
-def req_1(data_structs):
+
+def req_1(data_structs,lat1, long1, lat2, long2):
     """
     Función que soluciona el requerimiento 1
     """
-    # TODO: Realizar el requerimiento 1
-    pass
+    distancia_total = 0 
+    total_aeropuertos = 0
+    tiempo_trayecto = 0
+    aeropuertos_intermedios = lt.newList("ARRAY_LIST")
+    
+    hash_airports = data_structs["hash_airports"]
+    comercial_graph = data_structs["graph_comercial_distance"]
+    vertices = mp.valueSet(hash_airports)
+
+    vertice_origen_full = closest_vertx(vertices, lat1, long1)
+    vertice_destino_full = closest_vertx(vertices, lat2, long2)
+    vertice_origen = vertice_origen_full["ICAO"]
+    vertice_destino = vertice_destino_full["ICAO"]
+    search = dfs.DepthFirstSearch(comercial_graph, vertice_destino)
+    haspath = dfs.hasPathTo(search, vertice_origen)
+
+    if haspath:
+        camino = dfs.pathTo(search, vertice_origen)
+        prev = None
+        for vertice in lt.iterator(camino):
+            total_aeropuertos +=1
+            if prev is not None:
+                arco = gr.getEdge(comercial_graph, vertice, prev)
+                peso = arco["weight"]
+                distancia_total += peso
+                aeropuerto_info = me.getValue(mp.get(hash_airports, vertice))
+                vuelo_info = me.getValue(mp.get(data_structs["hash_routes"], vertice + "-" + prev + "-AVIACION_COMERCIAL"  ))
+                info_aeropuerto = {
+                        "ICAO": aeropuerto_info["ICAO"],
+                        "Nombre": aeropuerto_info["NOMBRE"],
+                        "Ciudad": aeropuerto_info["CIUDAD"],
+                        "Pais": aeropuerto_info["PAIS"]
+                }
+                tiempo_trayecto += vuelo_info["TIEMPO_VUELO"]
+                lt.addLast(aeropuertos_intermedios,info_aeropuerto)
+            else:
+                #Para el primer aeropuerto, porque prev = none
+                aeropuerto_info = me.getValue(mp.get(hash_airports, vertice))
+                info_aeropuerto = {
+                        "ICAO": aeropuerto_info["ICAO"],
+                        "Nombre": aeropuerto_info["NOMBRE"],
+                        "Ciudad": aeropuerto_info["CIUDAD"],
+                        "Pais": aeropuerto_info["PAIS"]
+                }
+                lt.addLast(aeropuertos_intermedios,info_aeropuerto)
+            prev = vertice
+    
+        
+    return aeropuertos_intermedios,distancia_total,total_aeropuertos , vertice_origen_full, vertice_destino_full, tiempo_trayecto
+
+            
 
 
 def req_2(data_structs):
@@ -123,8 +259,64 @@ def req_3(data_structs):
     """
     Función que soluciona el requerimiento 3
     """
-    # TODO: Realizar el requerimiento 3
-    pass
+  
+    trayectos = lt.newList("ARRAY_LIST")
+    
+    graph_comercial_distance = data_structs["graph_comercial_distance"]
+    
+    concurrency_comercial = data_structs["concurrency_comercial"]
+    mayor_concurrencia = lt.firstElement(concurrency_comercial)
+    
+    search = prim.PrimMST(graph_comercial_distance, mayor_concurrencia["ICAO"])
+    peso = prim.weightMST(graph_comercial_distance, search)
+
+    subgrafo = gr.newGraph(datastructure="ADJ_LIST", directed = True, cmpfunction = compare_id)
+    mst = search["mst"]
+    for minicamino in lt.iterator(mst):
+        add_vertice(subgrafo, minicamino["vertexA"])
+        add_vertice(subgrafo, minicamino["vertexB"])
+        add_arco(subgrafo, minicamino["vertexA"], minicamino["vertexB"], minicamino["weight"])
+    
+    sub_bfs = bfs.BreathFirstSearch(subgrafo, mayor_concurrencia["ICAO"])
+    
+    lt.deleteElement(concurrency_comercial,1)
+    
+    for aeropuerto in lt.iterator(concurrency_comercial):
+        tiene = bfs.hasPathTo(sub_bfs, aeropuerto["ICAO"])
+        if tiene:
+            camino = bfs.pathTo(sub_bfs, aeropuerto["ICAO"])
+            prev = None
+            for vertice in lt.iterator(camino):
+                if prev == None:
+                    prev = vertice
+                else:
+                    minipath = {"vertexA" : prev, "vertexB": vertice,"weight" : 0 }
+                    prev = vertice
+                    v1 = minipath["vertexA"]
+                    v2 = minipath["vertexB"]
+                    v1_info = me.getValue(mp.get(data_structs["hash_airports"], v1))
+                    v2_info = me.getValue(mp.get(data_structs["hash_airports"], v2))
+                    lat1_bono = float(v1_info["LATITUD"])
+                    long1_bono = float(v1_info["LONGITUD"])
+                    lat2_bono = float(v2_info["LATITUD"])
+                    long2_bono = float(v2_info["LONGITUD"])
+                    minipath["weight"] = haversine(lat1_bono, long1_bono ,lat2_bono, long2_bono)
+                    peso += minipath["weight"]
+                    vuelo_info = me.getValue(mp.get(data_structs["hash_routes"], v2 + "-" + v1 + "-AVIACION_COMERCIAL"  ))
+                    info_trayectos = {
+                        "Origen": v1,
+                        "Destino": v2,
+                        "weight": minipath["weight"],
+                        "Tiempo" : vuelo_info["TIEMPO_VUELO"]
+                    }
+                    lt.addLast(trayectos, info_trayectos)
+       
+    lt.addFirst(concurrency_comercial, mayor_concurrencia)
+    
+    return trayectos, peso, mayor_concurrencia
+
+
+
 
 
 def req_4(data_structs):
@@ -136,19 +328,51 @@ def req_4(data_structs):
 
 
 def req_5(data_structs):
-    """
-    Función que soluciona el requerimiento 5
-    """
-    # TODO: Realizar el requerimiento 5
     pass
 
 
-def req_6(data_structs):
+def req_6(data_structs, m_airports):
     """
     Función que soluciona el requerimiento 6
     """
     # TODO: Realizar el requerimiento 6
-    pass
+    list_concurrency_comercial = data_structs['concurrency_comercial']
+    most_important = lt.firstElement(list_concurrency_comercial)
+    graph_militar_distance = data_structs['graph_militar_distance']
+    dijkstra_militar = djk.Dijkstra(graph_militar_distance, most_important['ICAO'])
+    paths = lt.newList('ARRAY_LIST')
+    i = 2
+    
+    while lt.size(paths) < m_airports:
+        path = djk.pathTo(dijkstra_militar, lt.getElement(list_concurrency_comercial, i)['ICAO'] )
+        if path:
+            info_path = {
+                'total_airports': 1,
+                'airports': lt.newList('ARRAY_LIST'),
+                'flights': lt.newList('ARRAY_LIST'),
+                'total_distance': 0
+            }
+            for edge in lt.iterator(path):
+                dest = edge['vertexB']
+                source = edge['vertexA']
+                info_path['total_distance'] += edge['weight']
+                info_path['total_airports'] += 1
+                info_airport_dest = me.getValue(mp.get(data_structs['hash_airports'], dest))
+                info_airport_source = me.getValue(mp.get(data_structs['hash_airports'], source))
+                if lt.size(info_path['airports']) == 0:
+                    lt.addLast(info_path['airports'], info_airport_dest)                
+                lt.addLast(info_path['airports'], info_airport_source)
+                edge_info = {
+                    'destino': dest,
+                    'origen': source,
+                    'distancia': edge['weight']
+                }
+                lt.addLast(info_path['flights'], edge_info)
+            lt.addLast(paths, info_path)
+        i += 1
+  
+    return paths, most_important
+
 
 
 def req_7(data_structs):
@@ -159,43 +383,96 @@ def req_7(data_structs):
     pass
 
 
-def req_8(data_structs):
+
+
+#FUNCIONES AUXILIARES
+
+def sort_files(data_structs, type):
     """
-    Función que soluciona el requerimiento 8
+    Ordena los archivos de acuerdo a un criterio
     """
-    # TODO: Realizar el requerimiento 8
-    pass
+    if type == 'AVIACION_COMERCIAL':
+        merg.sort(data_structs, sort_concurrency_comercial)
+    elif type == 'AVIACION_CARGA':
+        merg.sort(data_structs, sort_concurrency_charge)
+    elif type == 'MILITAR':
+        merg.sort(data_structs, sort_concurrency_militar)
 
 
-# Funciones utilizadas para comparar elementos dentro de una lista
 
-def compare(data_1, data_2):
+def sort_concurrency_comercial(airport1, airport2):
     """
-    Función encargada de comparar dos datos
+    Ordena los aeropuertos por la cantidad de vuelos comerciales
     """
-    #TODO: Crear función comparadora de la lista
-    pass
-
-# Funciones de ordenamiento
-
-
-def sort_criteria(data_1, data_2):
-    """sortCriteria criterio de ordenamiento para las funciones de ordenamiento
-
-    Args:
-        data1 (_type_): _description_
-        data2 (_type_): _description_
-
-    Returns:
-        _type_: _description_
+    c1 = airport1['concurrency_comercial']
+    c2 = airport2['concurrency_comercial']
+    if c1 > c2:
+        return True
+    elif c1 < c2:
+        return False
+    else:
+        return airport1['ICAO'] < airport2['ICAO']
+    
+def sort_concurrency_charge(airport1, airport2):
     """
-    #TODO: Crear función comparadora para ordenar
-    pass
+    Ordena los aeropuertos por la cantidad de vuelos de carga
+    """
+    c1 = airport1['concurrency_charge']
+    c2 = airport2['concurrency_charge']
+    if c1 > c2:
+        return True
+    elif c1 < c2:
+        return False
+    else:
+        return airport1['ICAO'] < airport2['ICAO']
+
+def sort_concurrency_militar(airport1, airport2):
+    """
+    Ordena los aeropuertos por la cantidad de vuelos militares
+    """
+    c1 = airport1['concurrency_militar']
+    c2 = airport2['concurrency_militar']
+    if c1 > c2:
+        return True
+    elif c1 < c2:
+        return False
+    else:
+        return airport1['ICAO'] < airport2['ICAO']
+    
+    
+def add_vertice(grafo, id_v):
+    contains = gr.containsVertex(grafo, id_v)
+    if not contains:
+        gr.insertVertex(grafo, id_v)
+    return grafo
+
+def add_arco(grafo,v1,v2,weigth):
+    edge_entry = gr.getEdge(grafo,v1,v2) 
+    if edge_entry == None:
+        gr.addEdge(grafo,v1,v2,weigth)
+    return grafo
 
 
-def sort(data_structs):
-    """
-    Función encargada de ordenar la lista con los datos
-    """
-    #TODO: Crear función de ordenamiento
-    pass
+def compare_id(data_1, data_2):
+    
+    if data_1 > me.getKey(data_2):
+        return 1
+    elif data_1 < me.getKey(data_2):
+        return -1
+    else:
+        return 0
+    
+def closest_vertx(vertices, lat1, long1):
+    closest_vertx = None
+    min_distance = None
+    for vertice in lt.iterator(vertices):
+        lat = vertice["LATITUD"]
+        long = vertice["LONGITUD"]
+        new_distance = haversine( lat1, long1, lat, long )
+        if min_distance == None:
+            min_distance = new_distance
+        if new_distance < min_distance:
+            min_distance = new_distance
+            closest_vertx = vertice
+    return closest_vertx
+
--- App/view.py
+++ App/view.py
@@ -45,7 +45,8 @@ def new_controller():
         Se crea una instancia del controlador
     """
     #TODO: Llamar la función del controlador donde se crean las estructuras de datos
-    pass
+    control = controller.new_controller()
+    return control
 
 
 def print_menu():
@@ -67,23 +68,92 @@ def load_data(control):
     Carga los datos
     """
     #TODO: Realizar la carga de datos
-    pass
+    data = controller.load_data(control)
+    comercial = control['model']['concurrency_comercial']
+    charge = control['model']['concurrency_charge']
+    militar = control['model']['concurrency_militar']
+
+    print(f"Se cargaron {mp.size(data['model']['hash_airports'])} aeropuertos")
+    print(f"Se cargaron {mp.size(data['model']['hash_routes'])} vuelos")
+
+    print("\nAeropuertos de aviación comercial")
+    table_comercial = print_tabulate(comercial, ['NOMBRE', 'ICAO', 'CIUDAD', 'concurrency_comercial'])
+    print(table_comercial)
+
+    print("\nAeropuertos de aviación de carga")
+    table_charge = print_tabulate(charge, ['NOMBRE', 'ICAO', 'CIUDAD', 'concurrency_charge'])
+    print(table_charge)
+
+    print("\nAeropuertos de aviación militar")
+    table_militar = print_tabulate(militar, ['NOMBRE', 'ICAO', 'CIUDAD', 'concurrency_militar'])
+    print(table_militar)
+ 
 
 
-def print_data(control, id):
-    """
-        Función que imprime un dato dado su ID
-    """
-    #TODO: Realizar la función para imprimir un elemento
-    pass
+def print_tabulate(data_struct, columns):
+    data = data_struct
+
+    if lt.isEmpty(data):
+        return 'No hay datos'
+
+    #Filtrar solo ultimos y primeros 3 datos si es muy grande la lista
+    if lt.size(data_struct) > 10:
+        data = controller.get_first_last(data_struct)
+        print(f'Se encontraron {lt.size(data_struct)}  resultados. Imprimiendo primeros y ultimos 5...')
+
+    #Lista vacía para crear la tabla
+    reduced = []
+
+    #Iterar cada línea de la lista
+    for result in lt.iterator(data):
+        line = []
+        #Iterar las columnas para solo imprimir las deseadas
+        for column in columns:
+            line.append(result[column])
+        reduced.append(line)
+    table = tabulate(reduced, headers=columns, tablefmt="grid", maxcolwidths=30)
+    return table
+
+def print_tabulate_5(lista, columnas):
+    lista = lista["elements"][:40]
+    reduced = []
+    for result in lista:
+        linea = []
+        for c in columnas:
+            linea.append(result[c])
+        reduced.append(linea)  
+    tabla = print(tabulate(reduced, headers=columnas, tablefmt="grid"))
+    return tabla
+
+def print_tabulate_full(lista, columnas):
+    lista = lista["elements"]
+    reduced = []
+    for result in lista:
+        linea = []
+        for c in columnas:
+            linea.append(result[c])
+        reduced.append(linea)  
+    tabla = print(tabulate(reduced, headers=columnas, tablefmt="grid"))
+    return tabla
 
 def print_req_1(control):
     """
         Función que imprime la solución del Requerimiento 1 en consola
     """
-    # TODO: Imprimir el resultado del requerimiento 1
-    pass
-
+    latitud1 = float(input("Ingrese la latitud del punto de origen: "))
+    longitud1 = float(input("Ingrese la longitud del punto de origen: "))
+    latitud2 = float(input("Ingrese la latitud del punto de destino: "))
+    longitud2 = float(input("Ingrese la longitud del punto de destino: "))
+    aeropuertos_intermedios,distancia_total,total_aeropuertos , vertice_origen, vertice_destino, tiempo_trayecto = controller.req_1(control, latitud1, longitud1, latitud2, longitud2)
+    print("-------------------REQ 1 INFO -------------------")
+    print( "La distancia total es de: " + str(distancia_total) + " km")
+    print("El número de aeropuertos que se visitan en el camino encontrado es de: " + str(total_aeropuertos) + "\n")
+    print("-------------------INFO AEROPUETOS -------------------")
+    print("Origen: " + vertice_origen['ICAO'] + " - " + vertice_origen['NOMBRE'] + " - " + vertice_origen['CIUDAD'] + " - " + vertice_origen['PAIS'] )
+    print("Destino: " + vertice_destino['ICAO'] + " - " + vertice_destino['NOMBRE'] + " - " + vertice_destino['CIUDAD'] + " - " + vertice_destino['PAIS'] + "\n")
+    print("-------------------INFO SECUENCIA DE TRAYECTOS -------------------")
+    print_tabulate_full(aeropuertos_intermedios, ['ICAO',"Nombre",'Ciudad',"Pais"])
+    print("\n" + "El tiempo total del trayecto es de: " + str(tiempo_trayecto) + " minutos o " + str(tiempo_trayecto/60) + " horas"  + "\n")
 
 def print_req_2(control):
     """
@@ -98,9 +168,22 @@ def print_req_3(control):
         Función que imprime la solución del Requerimiento 3 en consola
     """
     # TODO: Imprimir el resultado del requerimiento 3
-    pass
-
-
+    
+    vertices, weight, mayor_concurrencia  = controller.req_3(control)
+    weight = round(weight,2)
+    print("-------------------REQ 3 INFO -------------------")
+    print("INFORMACION DEL AEROPUERTO MÁS IMPORTANTE SEGÚN LA CONCURRENCIA COMERCIAL: \n")
+    print("Identificador ICAO: " + mayor_concurrencia['ICAO'])
+    print("Nombre: " + mayor_concurrencia['NOMBRE'])
+    print("Ciudad: " + mayor_concurrencia['CIUDAD'])
+    print("pais: " + mayor_concurrencia['PAIS'])
+    print("Valor de concurrencia comercial: " + str(mayor_concurrencia['concurrency_comercial']))
+    print("Suma de la distancia total de los trayectos: " + str(weight) + " km")
+    print("Numero total de trayectos posible: " + str(len(vertices)) + "\n")
+    print("-------------------INFO SECUENCIA DE TRAYECTOS -------------------")
+    print_tabulate_5(vertices, ['Origen',"Destino",'weight',"Tiempo"])
+    
+    
 def print_req_4(control):
     """
         Función que imprime la solución del Requerimiento 4 en consola
@@ -113,7 +196,6 @@ def print_req_5(control):
     """
         Función que imprime la solución del Requerimiento 5 en consola
     """
-    # TODO: Imprimir el resultado del requerimiento 5
     pass
 
 
@@ -122,6 +204,23 @@ def print_req_6(control):
         Función que imprime la solución del Requerimiento 6 en consola
     """
     # TODO: Imprimir el resultado del requerimiento 6
+    m = input("Ingrese el número de aeropuertos a buscar: ")
+    paths, most_important = controller.req_6(control, int(m))
+    print("Aeropuerto con mayor concurrencia comercial: ")
+    print("ICAO: " + most_important['ICAO'] + "\n" + "Nombre: " + most_important['NOMBRE'] + "\n" + "Ciudad: " + most_important['CIUDAD'] + "\n" + "Pais: " + most_important['PAIS'] + "\n" + "Concurrencia: " + str(most_important['concurrency_comercial']) + "\n")
+    print("-"*20)
+    print("Rutas de los aeropuertos más importantes: \n")
+    i = 1
+    for info_path in lt.iterator(paths):
+        print("-"*100)
+        print("RUTA #" + str(i) + "\n")
+        print("Total de los aeropuertos en la ruta: " + str(info_path['total_airports']) + "\n")
+        print("Aeropuertos en la ruta: \n")
+        print_tabulate_5(info_path['airports'], ['ICAO', 'NOMBRE', 'CIUDAD', 'PAIS'])
+        print("\nVuelos en la ruta: \n")
+        print_tabulate_5(info_path['flights'], ['origen', 'destino', 'distancia', ])
+        print("\nTotal de distancia recorrida: " + str(info_path['total_distance']) + " KM\n")
+        i += 1
     pass
 
 
@@ -159,7 +258,6 @@ if __name__ == "__main__":
             data = load_data(control)
         elif int(inputs) == 2:
             print_req_1(control)
-
         elif int(inputs) == 3:
             print_req_2(control)
 
